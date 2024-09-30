1f351f501c7644a0b44778ab1bf18bddc9327e9b
mykcyeung <cheung625@hotmail.com>
2024-10-01 05:53:52 +0800

    first

---
+++ components/Grid.tsx
@@ -0,0 +1,38 @@
+import { section } from 'framer-motion/client'
+import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
+
+// interface GridItemProps {
+//   id: string;
+//   title: string;
+//   description: string;
+//   className: string;
+//   img: string;
+//   imgClassName: string;
+  
+// }
+
+const Grid = () => {
+  return (
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map(({id, title, description, className, img, imgClassName, spareImg, titleClassName}) => (
+          <BentoGridItem
+            key={id}
+            id={id}
+            title={title}
+            description={description}
+            className={className}
+            img={img}
+            imgClassName={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  )
+}
+
+export default Grid
